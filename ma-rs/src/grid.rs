77ec47edb6137fa8dfabd84ba205b90343659af3
Martin Hafskjold Thoresen <martin@vind.ai>
2023-11-26 21:23:55 +0100

    First attempt at parallelizing vineyards.
    Needing to reduce from scratch in all threads is bad, because we need
    to set up python every time.  Try to reduce from scratch only in the
    main thread, and then pass the result to the other threads.

--- ma-rs/src/grid.rs
+++ ma-rs/src/grid.rs
@@ -1,4 +1,9 @@
-use crate::complex::Pos;
+use std::collections::HashMap;
+
+use crate::{
+    complex::{Complex, Pos},
+    reduce_from_scratch, vineyards_123, Reduction, Swaps,
+};
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Index(pub [isize; 3]);
@@ -16,7 +21,7 @@ impl std::ops::Add<Index> for Index {
 
 impl pyo3::IntoPy<pyo3::PyObject> for Index {
     fn into_py(self, py: pyo3::Python<'_>) -> pyo3::PyObject {
-        pyo3::types::PyList::new(py, &self.0).into()
+        pyo3::types::PyTuple::new(py, &self.0).into()
     }
 }
 
@@ -148,39 +153,91 @@ impl Grid {
         }
     }
 
-    fn run_state(&mut self, max_volume: isize) {
-        if self.volume() <= max_volume {
-            println!("sleep {:?}", rayon::current_thread_index());
+    pub fn number_of_grid_edges(&self) -> isize {
+        self.shape.0[0] * self.shape.0[1] * (self.shape.0[2] - 1)
+            + self.shape.0[0] * (self.shape.0[1] - 1) * self.shape.0[2]
+            + (self.shape.0[0] - 1) * self.shape.0[1] * self.shape.0[2]
+    }
 
-            std::thread::sleep(std::time::Duration::from_millis(1000));
+    /// Run vineyards across all edges of the grid.  
+    fn run_vineyards_in_grid(
+        &self,
+        complex: &Complex,
+    ) -> (HashMap<Index, Reduction>, Vec<(Index, Index, Swaps)>) {
+        let mut hm = HashMap::new();
+        let mut all_swaps = Vec::new();
 
-            // let [w, h, d] = self.shape.0;
-            // p
-            // for i in 0..w {
-            //     for j in 0..h {
-            //         for k in 0..d {
-            //             self.cell_states
-            //                 .insert(Index([i, j, k]), format!("{} {} {}", i, j, k));
-            //         }
-            //     }
+        let mut edges_visited = 0;
+        let total_edges = self.number_of_grid_edges();
+
+        self.visit_edges(Index([0; 3]), |new_cell, old_cell| {
+            let last_percent = (100.0 * edges_visited as f64 / total_edges as f64).floor();
+            edges_visited += 1;
+            let this_percent = (100.0 * edges_visited as f64 / total_edges as f64).floor();
+            // if last_percent != this_percent {
+            //     eprint!(
+            //         "Progress: {}/{} ({}%)\r",
+            //         edges_visited, total_edges, this_percent
+            //     );
             // }
-        } else {
-            let (mut left, mut right, offset_index) = self.split_with_overlap();
-            rayon::join(
-                || left.run_state(max_volume),
-                || right.run_state(max_volume),
-            );
-            self.cell_states = left.cell_states;
-            for (k, v) in right.cell_states.into_iter() {
-                let k = k + offset_index;
-                self.cell_states.insert(k, v);
+
+            if let Some(old_cell) = old_cell {
+                let old_state = hm
+                    .get(&old_cell)
+                    .expect("prev_cell should have state in the map.");
+                let p = self.center(new_cell);
+                let (new_state, swaps) = vineyards_123(complex, old_state, p);
+                all_swaps.push((old_cell, new_cell, swaps));
+                hm.insert(new_cell, new_state);
+            } else {
+                let p = self.center(new_cell);
+                let state = reduce_from_scratch(complex, p);
+                hm.insert(new_cell, state);
+            }
+        });
+        (hm, all_swaps)
+    }
+
+    fn run_state(&mut self, max_volume: isize, complex: &Complex) -> Vec<(Index, Index, Swaps)> {
+        fn inner(
+            grid: &mut Grid,
+            max_volume: isize,
+            complex: &Complex,
+        ) -> Vec<(Index, Index, Swaps)> {
+            if grid.volume() <= max_volume {
+                let t0 = std::time::Instant::now();
+                let (_, swaps) = grid.run_vineyards_in_grid(complex);
+                let t1 = std::time::Instant::now();
+                let dt = t1 - t0;
+                let dt = dt.as_secs() as f64 + dt.subsec_nanos() as f64 * 1e-9;
+                eprintln!(
+                    "t{:02} run_vineyards_in_grid: {} seconds",
+                    rayon::current_thread_index().unwrap(),
+                    dt,
+                );
+                return swaps;
+            } else {
+                let (mut left, mut right, offset_index) = grid.split_with_overlap();
+                let (mut left_swaps, right_swaps) = rayon::join(
+                    || left.run_state(max_volume, complex),
+                    || right.run_state(max_volume, complex),
+                );
+
+                left_swaps.reserve(right_swaps.len());
+                for s in right_swaps {
+                    left_swaps.push((s.0 + offset_index, s.1 + offset_index, s.2));
+                }
+
+                return left_swaps;
             }
         }
+
+        pyo3::Python::with_gil(|py| py.allow_threads(|| inner(self, max_volume, complex)))
     }
 }
 
 impl Grid {
-    pub fn visit_edges<F: Fn(Index, Option<Index>)>(&self, start: Index, f: F) {
+    pub fn visit_edges<F: FnMut(Index, Option<Index>)>(&self, start: Index, mut f: F) {
         let mut queue = std::collections::VecDeque::new();
         queue.push_back((start, None));
 
